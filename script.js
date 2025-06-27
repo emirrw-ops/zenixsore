9e2fdfd93d276b97740df78e24520dff3d77273f
emirrw-ops <emirrk.qq@gmail.com>
2025-06-27 13:17:50 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,19 +1,1 @@
-document.addEventListener("DOMContentLoaded", function () {
-  const joinBtn = document.getElementById("joinBtn");
 
-  joinBtn.addEventListener("click", function (e) {
-    e.preventDefault(); // Sayfanın yeniden yüklenmesini engelle
-
-    const secim = prompt(
-      "Hangi sunucuya katılmak istiyorsun?\n\n1️⃣ Kod Sunucusu\n2️⃣ Owo Sunucusu\n\n(1 veya 2 yaz)"
-    );
-
-    if (secim === "1") {
-      window.open("https://discord.gg/HNMpeecPdB", "_blank");
-    } else if (secim === "2") {
-      window.open("https://discord.gg/owohunt", "_blank");
-    } else {
-      alert("Geçersiz seçim yaptın.");
-    }
-  });
-});
